69921e880903596d30f5d7dc7d2e86e0874bc6c4
David Monllao <david.monllao@gmail.com>
2017-12-28 17:59:34 +0100

    MNIST dataset added as a default.

--- README.md
+++ README.md
@@ -11,12 +11,17 @@ Tensorflow playground for comparing different algorithms performance.
 
 To view all configuration parameters:
 
-    python tfplayground/train.py -m
+    python tfplayground/train.py -h
 
 To run it using your supervised learning dataset (no categorical features support).
 
     python tfplayground/train.py /path/to/training1.csv /path/to/training2.csv /path/to/testing.csv \
     --n_samples=60000 --n_features=784 --n_classes=10
+
+    # Example using MNIST dataset.
+    tar -xzvf "data/mnist/mnist.tar.gz" -C data/mnist/
+    python tfplayground/train.py data/mnist/mnist_train.csv data/mnist/mnist_test.csv \
+        --n_samples=60000 --n_features=784 --n_classes=10 --label_first_column
 
 To view the results:
 
--- compare.sh
+++ compare.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Use the included MNIST dataset by default.
+TRAIN_FILE="data/mnist/mnist_train.csv"
+TEST_FILE="data/mnist/mnist_test.csv"
+
+# MNIST data.
+N_SAMPLES=60000
+N_FEATURES=784
+N_CLASSES=10
+
 ACTIVATION='tanh'
 NUM_EPOCH=30
 N_HIDDEN=100
@@ -7,11 +16,6 @@ KEEP_PROB=0.9 # Use 1. for no dropout regularization.
 L2_REG_TERM=0.01 # Use 0. for no l2 regularization.
 OPTIMIZER='adam'
 INPUT_NORM='l2'
-
-# MNIST data.
-N_SAMPLES=60000
-N_FEATURES=784
-N_CLASSES=10
 
 MIN_BATCH_SIZE=100
 MAX_BATCH_SIZE=10000
@@ -26,80 +30,80 @@ END_LEARNING_RATE=0.1
 
 set -e
 
+if [ ! -f "$TRAIN_FILE" ] || [ ! -f "$TEST_FILE" ]; then
+    if [ "$TRAIN_FILE" == "data/mnist/mnist_train.csv" ]; then
+        # Extract the included MNIST dataset.
+        tar -xzvf "data/mnist/mnist.tar.gz" -C data/mnist/
+    else
+        echo "Training or test file do not exist."
+        exit 1
+    fi
+fi
+
 python clear_summaries.py 1> /dev/null
 
 # Log increment from $MIN_BATCH_SIZE to $MAX_BATCH_SIZE.
-python tfplayground/research/varying_batch_size.py /home/davidm/Desktop/mnist_data/mnist_train.csv \
-    /home/davidm/Desktop/mnist_data/mnist_test.csv \
+python tfplayground/research/varying_batch_size.py "$TRAIN_FILE" "$TEST_FILE" \
     -m=$N_SAMPLES -n=$N_FEATURES -c=$N_CLASSES -l -e=$NUM_EPOCH -nh=$N_HIDDEN \
     -minb=$MIN_BATCH_SIZE -maxb=$MAX_BATCH_SIZE -slr=$START_LEARNING_RATE -elr=$END_LEARNING_RATE -a="$ACTIVATION" \
     -d="$KEEP_PROB" -o=$OPTIMIZER -i='log_increase' --norm="$INPUT_NORM" \
     -l2="$L2_REG_TERM"
 
 # Linear increment from $MIN_BATCH_SIZE to $MAX_BATCH_SIZE.
-python tfplayground/research/varying_batch_size.py /home/davidm/Desktop/mnist_data/mnist_train.csv \
-    /home/davidm/Desktop/mnist_data/mnist_test.csv \
+python tfplayground/research/varying_batch_size.py "$TRAIN_FILE" "$TEST_FILE" \
     -m=$N_SAMPLES -n=$N_FEATURES -c=$N_CLASSES -l -e=$NUM_EPOCH -nh=$N_HIDDEN \
     -minb=$MIN_BATCH_SIZE -maxb=$MAX_BATCH_SIZE -slr=$START_LEARNING_RATE -elr=$END_LEARNING_RATE -a="$ACTIVATION" \
     -d="$KEEP_PROB" -o=$OPTIMIZER -i='linear_increase' --norm="$INPUT_NORM" \
     -l2="$L2_REG_TERM"
 
 # Linear decrease from $MAX_BATCH_SIZE to $MIN_BATCH_SIZE.
-python tfplayground/research/varying_batch_size.py /home/davidm/Desktop/mnist_data/mnist_train.csv \
-    /home/davidm/Desktop/mnist_data/mnist_test.csv \
+python tfplayground/research/varying_batch_size.py "$TRAIN_FILE" "$TEST_FILE" \
     -m=$N_SAMPLES -n=$N_FEATURES -c=$N_CLASSES -l -e=$NUM_EPOCH -nh=$N_HIDDEN \
     -minb=$MIN_BATCH_SIZE -maxb=$MAX_BATCH_SIZE -slr=$START_LEARNING_RATE -elr=$END_LEARNING_RATE -a="$ACTIVATION" \
     -d="$KEEP_PROB" -o=$OPTIMIZER -i='linear_decrease' --norm="$INPUT_NORM" \
     -l2="$L2_REG_TERM"
 
 # Logarithmic decrease from $MAX_BATCH_SIZE to $MIN_BATCH_SIZE.
-python tfplayground/research/varying_batch_size.py /home/davidm/Desktop/mnist_data/mnist_train.csv \
-    /home/davidm/Desktop/mnist_data/mnist_test.csv \
+python tfplayground/research/varying_batch_size.py "$TRAIN_FILE" "$TEST_FILE" \
     -m=$N_SAMPLES -n=$N_FEATURES -c=$N_CLASSES -l -e=$NUM_EPOCH -nh=$N_HIDDEN \
     -minb=$MIN_BATCH_SIZE -maxb=$MAX_BATCH_SIZE -slr=$START_LEARNING_RATE -elr=$END_LEARNING_RATE -a="$ACTIVATION" \
     -d="$KEEP_PROB" -o=$OPTIMIZER -i='log_decrease' --norm="$INPUT_NORM" \
     -l2="$L2_REG_TERM"
 
 # Exponential increment from $MIN_BATCH_SIZE to $MAX_BATCH_SIZE.
-python tfplayground/research/varying_batch_size.py /home/davidm/Desktop/mnist_data/mnist_train.csv \
-    /home/davidm/Desktop/mnist_data/mnist_test.csv \
+python tfplayground/research/varying_batch_size.py "$TRAIN_FILE" "$TEST_FILE" \
     -m=$N_SAMPLES -n=$N_FEATURES -c=$N_CLASSES -l -e=$NUM_EPOCH -nh=$N_HIDDEN \
     -minb=$MIN_BATCH_SIZE -maxb=$MAX_BATCH_SIZE -slr=$START_LEARNING_RATE -elr=$END_LEARNING_RATE -a="$ACTIVATION" \
     -d="$KEEP_PROB" -o=$OPTIMIZER -i='exp_increase' --norm="$INPUT_NORM" \
     -l2="$L2_REG_TERM"
 
 # Exponential decrease from $MAX_BATCH_SIZE to $MIN_BATCH_SIZE.
-python tfplayground/research/varying_batch_size.py /home/davidm/Desktop/mnist_data/mnist_train.csv \
-    /home/davidm/Desktop/mnist_data/mnist_test.csv \
+python tfplayground/research/varying_batch_size.py "$TRAIN_FILE" "$TEST_FILE" \
     -m=$N_SAMPLES -n=$N_FEATURES -c=$N_CLASSES -l -e=$NUM_EPOCH -nh=$N_HIDDEN \
     -minb=$MIN_BATCH_SIZE -maxb=$MAX_BATCH_SIZE -slr=$START_LEARNING_RATE -elr=$END_LEARNING_RATE -a="$ACTIVATION" \
     -d="$KEEP_PROB" -o=$OPTIMIZER -i='exp_decrease' --norm="$INPUT_NORM" \
     -l2="$L2_REG_TERM"
 
 # Fixed batch size $MIN_BATCH_SIZE
-python tfplayground/train.py /home/davidm/Desktop/mnist_data/mnist_train.csv \
-    /home/davidm/Desktop/mnist_data/mnist_test.csv \
+python tfplayground/train.py "$TRAIN_FILE" "$TEST_FILE" \
     -m=$N_SAMPLES -n=$N_FEATURES -c=$N_CLASSES -l -o=$OPTIMIZER -e=$NUM_EPOCH \
     -nh=$N_HIDDEN -b=$MIN_BATCH_SIZE -slr=$START_LEARNING_RATE -elr=$END_LEARNING_RATE -a="$ACTIVATION" \
     -d="$KEEP_PROB" --norm="$INPUT_NORM" -l2="$L2_REG_TERM"
 
 # Fixed batch size $MID_BATCH_SIZE
-python tfplayground/train.py /home/davidm/Desktop/mnist_data/mnist_train.csv \
-    /home/davidm/Desktop/mnist_data/mnist_test.csv \
+python tfplayground/train.py "$TRAIN_FILE" "$TEST_FILE" \
     -m=$N_SAMPLES -n=$N_FEATURES -c=$N_CLASSES -l -o=$OPTIMIZER -e=$NUM_EPOCH \
     -nh=$N_HIDDEN -b=$MID_BATCH_SIZE -slr=$START_LEARNING_RATE -elr=$END_LEARNING_RATE -a="$ACTIVATION" \
     -d="$KEEP_PROB" --norm="$INPUT_NORM" -l2="$L2_REG_TERM"
 
 # Fixed to $MAX_BATCH_SIZE.
-python tfplayground/train.py /home/davidm/Desktop/mnist_data/mnist_train.csv \
-    /home/davidm/Desktop/mnist_data/mnist_test.csv \
+python tfplayground/train.py "$TRAIN_FILE" "$TEST_FILE" \
     -m=$N_SAMPLES -n=$N_FEATURES -c=$N_CLASSES -l -o=$OPTIMIZER -e=$NUM_EPOCH \
     -nh=$N_HIDDEN -b=$MAX_BATCH_SIZE -slr=$START_LEARNING_RATE -elr=$END_LEARNING_RATE -a="$ACTIVATION" \
     -d="$KEEP_PROB" --norm="$INPUT_NORM" -l2="$L2_REG_TERM"
 
 # Fixed batch size of all training set.
-python tfplayground/train.py /home/davidm/Desktop/mnist_data/mnist_train.csv \
-    /home/davidm/Desktop/mnist_data/mnist_test.csv \
+python tfplayground/train.py "$TRAIN_FILE" "$TEST_FILE" \
     -m=$N_SAMPLES -n=$N_FEATURES -c=$N_CLASSES -l -o=$OPTIMIZER -e=$NUM_EPOCH \
     -nh=$N_HIDDEN -b=$N_SAMPLES -slr=$START_LEARNING_RATE -elr=$END_LEARNING_RATE -a="$ACTIVATION" \
     -d="$KEEP_PROB" --norm="$INPUT_NORM" -l2="$L2_REG_TERM"
